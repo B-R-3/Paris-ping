90a97e6b4a4168ef4887a23d2a3fc2b8aa3746d1
B-R-3 <brandon.molinaserna@limayrac.fr>
2025-11-06 20:05:59 +0100

    vitt

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/Paris-ping_build/",
   server: {
     host: "::",
     port: 8080,
